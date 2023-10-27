a504ac634ccc2ff46adb9669affab50f5a076d27
hyunsik jung <ball1241@naver.com>
2023-10-27 18:12:30 +0900

    [Fix] :영어 한글로 번역 및 주석처리

--- app/src/main/java/com/nbcamp_14_project/favorite/FavoriteListAdapter.kt
+++ app/src/main/java/com/nbcamp_14_project/favorite/FavoriteListAdapter.kt
@@ -8,7 +8,6 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.nbcamp_14_project.Utils
-import com.nbcamp_14_project.databinding.FragmentFavoriteBinding
 import com.nbcamp_14_project.databinding.ItemRvNewsMainBinding
 import com.nbcamp_14_project.detail.DetailInfo
 import com.nbcamp_14_project.detail.DetailViewModel
@@ -16,47 +15,47 @@ import java.util.Date
 
 class FavoriteListAdapter(private val onItemClick: (DetailInfo) -> Unit) : ListAdapter<DetailInfo, FavoriteListAdapter.ViewHolder>(
     object : DiffUtil.ItemCallback<DetailInfo>() {
-        override fun areItemsTheSame(
-            oldItem: DetailInfo, newItem: DetailInfo
-        ): Boolean {
+        // 아이템이 동일한지 비교하기 위한 콜백
+        override fun areItemsTheSame(oldItem: DetailInfo, newItem: DetailInfo): Boolean {
+            // 아이템의 고유 식별자인 제목을 비교
             return oldItem.title == newItem.title
         }
 
-        override fun areContentsTheSame(
-            oldItem: DetailInfo,
-            newItem: DetailInfo
-        ): Boolean {
+        // 아이템 내용이 동일한지 비교하기 위한 콜백
+        override fun areContentsTheSame(oldItem: DetailInfo, newItem: DetailInfo): Boolean {
+            // 아이템 자체를 비교
             return oldItem == newItem
         }
     }
 ) {
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): FavoriteListAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // 뷰홀더 생성
         return ViewHolder(
             ItemRvNewsMainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // 뷰홀더에 아이템 데이터 바인딩 및 아이템 클릭 핸들링
         val item = getItem(position)
         holder.bind(item)
         holder.itemView.setOnClickListener {
+            // 아이템 클릭 시 onItemClick 함수 호출
             onItemClick(item)
         }
     }
 
-    class ViewHolder(private val binding: ItemRvNewsMainBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private val binding: ItemRvNewsMainBinding) : RecyclerView.ViewHolder(binding.root) {
+        // 뷰홀더 클래스 정의
 
         fun bind(item: DetailInfo) = with(binding) {
-            title.text = item.title
-            ivThumbnail.load(item.thumbnail)
+            // 아이템 데이터를 뷰에 바인딩
+            title.text = item.title // 제목 설정
+            ivThumbnail.load(item.thumbnail) // 이미지 불러오기
             val date = Date(item.pubDate)
-            val value = date.time?.let { Utils.calculationTime(it) }
-            pubDate.text = item.author + " 기자" + " · " + value + " 작성"
+            val value = date.time?.let { Utils.calculationTime(it) } // 날짜 계산
+            pubDate.text = "${item.author} 기자 · $value 작성" // 작성자와 날짜 설정
         }
     }
 }
--- app/src/main/java/com/nbcamp_14_project/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/nbcamp_14_project/favorite/FavoriteViewModel.kt
@@ -7,51 +7,60 @@ import com.nbcamp_14_project.detail.DetailInfo
 import com.nbcamp_14_project.home.HomeModel
 
 class FavoriteViewModel : ViewModel() {
+    // ViewModel 클래스 정의
 
     private val _favoriteList: MutableLiveData<List<DetailInfo>> = MutableLiveData()
     val favoriteList: LiveData<List<DetailInfo>> get() = _favoriteList
 
     var isLogin: Boolean = false
+    // 사용자 로그인 상태를 저장하는 변수
 
+    // 즐겨찾기 아이템 추가
     fun addFavoriteItem(item: DetailInfo) {
         val currentList = _favoriteList.value?.toMutableList() ?: mutableListOf()
-        currentList.add(0, item)
-        _favoriteList.value = currentList
+        // 현재의 즐겨찾기 목록을 가져오거나, 비어있는 리스트를 생성
+        currentList.add(0, item) // 새로운 아이템을 목록의 맨 앞에 추가
+        _favoriteList.value = currentList // 변경된 목록을 LiveData에 설정
     }
 
+    // 즐겨찾기 아이템 삭제
     fun removeFavoriteItem(item: DetailInfo) {
         val currentList = _favoriteList.value?.toMutableList() ?: return
+        // 현재의 즐겨찾기 목록을 가져오거나, 목록이 비어있으면 함수 종료
         if (currentList.contains(item)) {
-            currentList.remove(item)
-            _favoriteList.value = currentList
+            currentList.remove(item) // 목록에서 해당 아이템 삭제
+            _favoriteList.value = currentList // 변경된 목록을 LiveData에 설정
         }
-
-
     }
-    fun removeFavoriteItemToPosition(item: DetailInfo){
+
+    // 즐겨찾기 아이템을 지정된 위치에서 삭제
+    fun removeFavoriteItemToPosition(item: DetailInfo) {
         val currentList = _favoriteList.value?.toMutableList() ?: return
-        fun findIndex(item: DetailInfo?):Int{
-            if(item == null) return 0
-            val findItem = currentList.find{
+        // 현재의 즐겨찾기 목록을 가져오거나, 목록이 비어있으면 함수 종료
+        fun findIndex(item: DetailInfo?): Int {
+            if (item == null) return 0
+            // 입력된 아이템이 null이면 0 반환
+            val findItem = currentList.find {
                 it.thumbnail == item.thumbnail
             }
+            // 현재 목록에서 아이템을 고유 식별자로 찾음
             return currentList.indexOf(findItem)
+            // 아이템의 위치를 반환
         }
         val findPosition = findIndex(item)
-        if(findPosition < 0){
-            return
+        if (findPosition < 0) {
+            return // 찾은 위치가 0 미만이면 함수 종료
         }
-        if(item == null) return
+        if (item == null) return
+        // 아이템이 null이면 함수 종료
         currentList.removeAt(findPosition)
+        // 아이템을 찾은 위치에서 삭제
         _favoriteList.value = currentList
-
+        // 변경된 목록을 LiveData에 설정
     }
 
+    // 즐겨찾기 목록을 설정
     fun setFavoriteList(list: List<DetailInfo>) {
         _favoriteList.value = list
     }
-
-
-
 }
-
