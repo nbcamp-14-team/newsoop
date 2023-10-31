1743ab972681357bd5b134b2e6bb5c26bdd39830
rising111 <ejttotto1234@gmail.com>
2023-10-31 20:37:42 +0900

    [FIX]: 리사이클러뷰 로딩 안전장치 구현

--- app/src/main/java/com/nbcamp_14_project/home/HomeFragment.kt
+++ app/src/main/java/com/nbcamp_14_project/home/HomeFragment.kt
@@ -18,20 +18,25 @@ import com.nbcamp_14_project.mainpage.MainActivity
 import kotlinx.coroutines.delay
 
 class HomeFragment(query:String) : Fragment() {
-    companion object {
-        fun newInstance() = HomeFragment("정치")
-    }
-    private var queryInbox = query //검색어 변수
 
+
+    companion object {
+        fun newInstance(query: String) = HomeFragment(query)
+    }
+
+    private var queryInbox = query //검색어 변수
+    var isLoading = false
 
     private var _binding: FragmentMainBinding? = null
     private val detailViewModel: DetailViewModel by activityViewModels()
     private val binding get() = _binding!!
+
     // HomeFragment Fragment
     private val headLineAdapter by lazy {
         HomeHeadLineAdapter(//ViewPager2에서 클릭이 일어났을 때, Detail에 데이터값을 보냄
-            onClick = {item ->
-                val detailInfo = item.toDetailInfo()//클릭된 아이템의 data class를 toDetailInfo data class로 수정
+            onClick = { item ->
+                val detailInfo =
+                    item.toDetailInfo()//클릭된 아이템의 data class를 toDetailInfo data class로 수정
                 detailViewModel.setDetailInfo(detailInfo)//뷰모델로 전송
                 val mainActivity = (activity as MainActivity)
                 mainActivity.runDetailFragment()//DetailFragment 실행
@@ -41,7 +46,8 @@ class HomeFragment(query:String) : Fragment() {
     private val newsAdapter by lazy {
         HomeNewsAdapter(//recyclerView에서 클릭이 일어났을 때, Detail에 데이터값을 보냄
             onClick = { item ->
-                val detailInfo = item.toDetailInfo()//클릭된 아이템의 data class를 toDetailInfo data class로 수정
+                val detailInfo =
+                    item.toDetailInfo()//클릭된 아이템의 data class를 toDetailInfo data class로 수정
                 detailViewModel.setDetailInfo(detailInfo)//뷰모델로 전송
                 val mainActivity = (activity as MainActivity)
                 mainActivity.runDetailFragment()//DetailFragment 실행
@@ -53,7 +59,8 @@ class HomeFragment(query:String) : Fragment() {
             this, HomeModelFactory()
         )[HomeViewModel::class.java]
     }
-    private var startingNum:Int = 6// 인피니티스크롤 시작지점 지정 변수
+    private var startingNum: Int = 6// 인피니티스크롤 시작지점 지정 변수
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -65,20 +72,21 @@ class HomeFragment(query:String) : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("query","$queryInbox")
+        Log.d("query", "$queryInbox")
         initView()//화면 설정 함수
         initViewModel()//뷰모델 설정 함수
         viewPagerViewModel.headLineNews(queryInbox)//메인 ViewPager에 헤드라인 뉴스 출력
         viewPagerViewModel.detailNews(queryInbox)//하단 리사이클러뷰에 뉴스 출력
 
     }
+
     fun initView() = with(binding) {
         category.text = queryInbox //
         val slideImageHandler: Handler = Handler()
         val slideImageRunnable = Runnable {// 메인 ViewPager 자동 스와이프 관련 runnable 함수
 
             rvMainNews.currentItem = rvMainNews.currentItem + 1
-            if (rvMainNews.currentItem  == headLineAdapter.itemCount) {
+            if (rvMainNews.currentItem == headLineAdapter.itemCount) {
                 rvMainNews.currentItem = 0
             }
         }// viewpager의 Item Position을 1 추가하여 페이지 이동하게 구현
@@ -99,23 +107,46 @@ class HomeFragment(query:String) : Fragment() {
                 }
             })
         }
-        rvMainNewsIndicator.setViewPager2(rvMainNews)
+        rvMainNewsIndicator.attachTo(rvMainNews)
         /**
          * 하단 리사이클러뷰 세팅
          */
 
         rvNews.adapter = newsAdapter//어뎁터 연결
-        rvNews.addOnScrollListener(object :RecyclerView.OnScrollListener(){//인피니티 스크롤 구현
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {//스크롤 상태가 변화할 때,
-                super.onScrollStateChanged(recyclerView, newState)
-                val lastVisiblePosition = (rvNews.layoutManager as LinearLayoutManager)!!.findLastCompletelyVisibleItemPosition()//마지막으로 보인 Item에 포지션변수
+        rvNews.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            //인피니티 스크롤 구현
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val lastVisiblePosition =
+                    (rvNews.layoutManager as LinearLayoutManager)!!.findLastCompletelyVisibleItemPosition()//마지막으로 보인 Item에 포지션변수
                 val itemCount = newsAdapter.itemCount - 1//Adapter아이템 개수
-                Log.d("VisiblePosition","$lastVisiblePosition + $itemCount")
-
-                if(!rvNews.canScrollHorizontally(1) && lastVisiblePosition == itemCount){
-                    Log.d("query1","$queryInbox")
-                    infinityAddNews(queryInbox)
+                Log.d("LoadingView","${viewPagerViewModel.isLoading}")
+                if (viewPagerViewModel.isLoading) {
+                    isLoading = false
                 }
+                Log.d("VisiblePosition", "$lastVisiblePosition + $itemCount + $isLoading")
+                if (!isLoading) {
+
+                    if (!rvNews.canScrollHorizontally(1) && (rvNews.layoutManager as LinearLayoutManager)!!.findLastCompletelyVisibleItemPosition() == itemCount) {
+                        isLoading = true
+                        infinityAddNews(queryInbox)
+                    }
+                }
+
+
+            }
+
+            override fun onScrollStateChanged(
+                recyclerView: RecyclerView,
+                newState: Int
+            ) {//스크롤 상태가 변화할 때,
+                super.onScrollStateChanged(recyclerView, newState)
+
+//
+//                if(!rvNews.canScrollHorizontally(1) && lastVisiblePosition == itemCount){
+//                    Log.d("query1","$queryInbox")
+//
+//                }
             }
         }
         )
@@ -133,9 +164,11 @@ class HomeFragment(query:String) : Fragment() {
             }
         }
     }
-    private fun infinityAddNews(query:String?){
-        viewPagerViewModel.detailNews(query!!,startingNum)
+
+    private fun infinityAddNews(query: String?) {
+        viewPagerViewModel.detailNewsInfinity(query!!, startingNum)
         startingNum += 6
+        viewPagerViewModel.isLoading = false
     }
 }
 
