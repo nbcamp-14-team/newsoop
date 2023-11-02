a10abea230e43b3568ccb5310e12882bb1a8c16a
Seulbi-Lee-project <ifssfws@gmail.com>
2023-11-02 12:45:44 +0900

    [DESIGN] : 설정페이지 UI 만들기 즐겨찾기 페이지에 설정페이지로 가는 버튼 만들기

---
+++ app/src/main/java/com/nbcamp_14_project/setting/SettingActivity.kt
@@ -0,0 +1,17 @@
+package com.nbcamp_14_project.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nbcamp_14_project.databinding.ActivitySettingBinding
+
+class SettingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
