d63479249e7768e749624714912ac864adf028e5
Seulbi-Lee-project <ifssfws@gmail.com>
2023-11-02 15:28:30 +0900

    [FEAT] : 설정에서 오픈소스 라이센스 확인 기능 추가

--- app/src/main/java/com/nbcamp_14_project/setting/SettingActivity.kt
+++ app/src/main/java/com/nbcamp_14_project/setting/SettingActivity.kt
@@ -3,6 +3,7 @@ package com.nbcamp_14_project.setting
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.nbcamp_14_project.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
@@ -26,6 +27,11 @@ class SettingActivity : AppCompatActivity() {
             referenceMail.setOnClickListener {
                 val intent = Intent(this@SettingActivity, EmailFormActivity::class.java)
                 startActivity(intent)
+            }
+
+            openSourceLicence.setOnClickListener {
+                OssLicensesMenuActivity.setActivityTitle("오픈소스 라이선스 목록")
+                startActivity(Intent(this@SettingActivity, OssLicensesMenuActivity::class.java))
             }
         }
     }
