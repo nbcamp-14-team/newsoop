5f36aa12b7018c95241f46de46269bb5240c7297
Seulbi-Lee-project <ifssfws@gmail.com>
2023-11-02 13:54:12 +0900

    [DESIGN] : 문의 메일 보내기 UI 작성

---
+++ app/src/main/java/com/nbcamp_14_project/setting/EmailFormActivity.kt
@@ -0,0 +1,12 @@
+package com.nbcamp_14_project.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nbcamp_14_project.R
+
+class EmailFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_form)
+    }
+}
