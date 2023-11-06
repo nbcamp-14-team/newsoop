960075e81a544771fad14a434aa99391c69ff4ac
rising111 <ejttotto1234@gmail.com>
2023-11-06 17:18:15 +0900

    [FIX] : 홈 프래그먼트 추천 탭 수정

--- app/src/main/java/com/nbcamp_14_project/home/HomeViewPagerAdapter.kt
+++ app/src/main/java/com/nbcamp_14_project/home/HomeViewPagerAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class HomeViewPagerAdapter(fragment: FragmentActivity):FragmentStateAdapter(fragment) {
+class HomeViewPagerAdapter(fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmentActivity) {
     var list = mutableListOf<HomeFragmentTabs>()
     private val listIds = list.map{it.hashCode().toLong()}
     override fun getItemCount(): Int {
--- app/src/main/java/com/nbcamp_14_project/home/HomeViewPagerRepositiory.kt
+++ app/src/main/java/com/nbcamp_14_project/home/HomeViewPagerRepositiory.kt
@@ -2,13 +2,10 @@ package com.nbcamp_14_project.home
 
 class HomeViewPagerRepository {
     private val list = mutableListOf<HomeFragmentTabs>(
-        HomeFragmentTabs(HomeFragment("정치"),"정치"),
+        HomeFragmentTabs(HomeFragment("정치 사회"),"정치/사회"),
         HomeFragmentTabs(HomeFragment("경제"),"경제"),
-        HomeFragmentTabs(HomeFragment("사회"),"사회"),
-        HomeFragmentTabs(HomeFragment("생활"),"생활"),
-        HomeFragmentTabs(HomeFragment("문화"),"문화"),
-        HomeFragmentTabs(HomeFragment("IT"),"IT"),
-        HomeFragmentTabs(HomeFragment("과학"),"과학"),
+        HomeFragmentTabs(HomeFragment("생활 문화"),"생활/문화"),
+        HomeFragmentTabs(HomeFragment("IT 과학"),"IT/과학"),
         HomeFragmentTabs(HomeFragment("세계"),"세계"),
     )
     fun addListAtFront(query:String,title:String): MutableList<HomeFragmentTabs> {
