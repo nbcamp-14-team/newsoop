5acedc55c021fe9a049350b5bf1fee910b8fbf9b
rising111 <ejttotto1234@gmail.com>
2023-11-14 16:05:55 +0900

    [FIX] : 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,15 +15,15 @@ android {
         applicationId = "com.nbcamp_14_project"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
