195c16ebae926717bdbe61b53eed1db7e9cd9e71
Seulbi-Lee-project <ifssfws@gmail.com>
2023-11-17 20:52:58 +0900

    [FIX] 개인정보 이용 약관 추가

--- app/src/main/java/com/nbcamp_14_project/setting/SettingActivity.kt
+++ app/src/main/java/com/nbcamp_14_project/setting/SettingActivity.kt
@@ -2,13 +2,17 @@ package com.nbcamp_14_project.setting
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.nbcamp_14_project.databinding.ActivitySettingBinding
+import com.nbcamp_14_project.newspaper.NewspaperDialog
 
 class SettingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingBinding
+    private var checkDeveloper = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +36,21 @@ class SettingActivity : AppCompatActivity() {
             openSourceLicence.setOnClickListener {
                 OssLicensesMenuActivity.setActivityTitle("오픈소스 라이선스 목록")
                 startActivity(Intent(this@SettingActivity, OssLicensesMenuActivity::class.java))
+            }
+
+            terms.setOnClickListener {
+                val link = "https://tall-button-e3d.notion.site/a16106c5e59d4985ae10f365a4a66d03"
+                startActivity(NewspaperDialog.newInstance(this@SettingActivity, link))
+            }
+
+            developer.setOnClickListener {
+                if(!checkDeveloper){
+                    developerText.visibility = View.VISIBLE
+                    checkDeveloper = true
+                }else{
+                    developerText.visibility = View.GONE
+                    checkDeveloper = false
+                }
             }
         }
     }
